d55aa544ea9dcc472cda450892f0fd4e91b02caa
zgfgNew <ivo.penzar+PaySafeCard-test@soar.hr>
2026-06-26 10:03:20 +0200

    Add files via upload

--- PIF/pif-test-json-files.sh
+++ PIF/pif-test-json-files.sh
@@ -10,12 +10,9 @@ echo "Crawl recursively and test custom JSON files for PIF module\
 FOLDERS_DEPTH="4"             # Increase for crawling deeper than to sub-sub-sub-sub-folders 
 
 TEST_PI="TEST_PIC"            # For testing PI with Play Integrity Checker 
+TEST_PI="TEST_MACHECK"           # For testing PI with Ma Integrity Check
+TEST_PI="TEST_PICHECK"       # For testing PI with Play Integrity API Checker
 TEST_PI="TEST_SPIC"           # Default, for testing PI with Simple Play Integrity Checker
-#TEST_PI="TEST_PICHECK"       # For testing PI with Play Integrity API Checker
-
-# No more using these checkers:
-#TEST_PI="TEST_AIC"           # For testing PI with Android Integrity Checker
-#TEST_PI="TEST_IAPI"          # For testing PI with Integrity API
 
 #MIGRATE_JSON="yes"           # Enable to pre-process JSON files by migrate.sh script from Osm0sis, migration script must be in this script's folder or in the PI Fork module's folder
 #DELETE_FAILED="yes"          # Enable to automatically delete JSON files that fail to pass Device Integrity
@@ -24,28 +21,29 @@ PIF_JSON="*pif.json"          # Default, PIF JSON mask to test all *.json files
 #PIF_JSON="custom.pif.json"   # PIF JSON mask to test only custom.pif.json files
 PI_WAIT_VERDICT="15"          # Play Integrity verdict usually returns in 5-6 seconds, but if you decrease and verdict arrives later, script will miss to read
 
-#DISPLAY_FACTOR_PERCENT="95"  # Low Resolution Display adjustement factor for all coordinates
-DISPLAY_FACTOR_PERCENT="100"  # Default Display adjustement factor for all coordinates
-#DISPLAY_FACTOR_PERCENT="115" # High Resolution Display adjustement factor for all coordinates
+DISPLAY_FACTOR_PERCENT="98" # Low resolution (1080x2340) Display adjustment factor for coordinates
+DISPLAY_FACTOR_PERCENT="100"  # Default resolution (1080x2400) Display adjustment factor for coordinates
+#DISPLAY_FACTOR_PERCENT="113" # High resolution (1220x2712) Display adjustment factor for coordinates
 
 
 # Change the code beyond this line only on your own risk
 
-X_COORD="550"                 # x coordinate for Check buttons in SPIC, PI API Checker, TB-Checker, Android Integrity Checker and Integrity API
-SWIPE_Y_COORD="1560"          # y coordinate for swiping the screen
+X_COORD="535"                 # x coordinate for Check buttons in SPIC, PI API Checker, TB-Checker, Android Integrity Checker and Integrity API
+SWIPE_Y_COORD="1000"          # y coordinate for swiping the screen
 
-PIC_Y_COORD="980"             # y coordinate for "Check" button in PI API Checker
-PIC_V_X_COORD="875"           # x coordinate for "Show Verdict" button in PI API Checker
-PIC_V_Y_COORD="200"           # y coordinate for "Show Verdict" button in PI API Checker
+PIC_Y_COORD="985"             # y coordinate for "Check" button in PI API Checker 975, 985, 1065 (0.99, 1, 1.08)
+PIC_V_X_COORD="875"           # x coordinate for "Show Verdict" button in PI API Checker 875, 875, 990 (1, 1, 1.13)
+PIC_V_Y_COORD="200"           # y coordinate for "Show Verdict" button in PI API Checker 195, 200, 225 (0.98, 1, 1.12)
 
-SPIC_Y_COORD="940"            # y coordinate for "Make Play Integrity request" button in SPIC
+SPIC_Y_COORD="945"            # y coordinate for "Make Play Integrity request" button in SPIC 935, 945, 1045 (0.99, 1, 1.11)
 
-PICHECK_Y_COORD="1545"        # y coordinate for "Check" button in PI API Checker
-PICHECK_V_X_COORD="890"       # x coordinate for "Show Verdict" button in PI API Checker
-PICHECK_V_Y_COORD="180"       # y coordinate for "Show Verdict" button in PI API Checker
+PICHECK_Y_COORD="1550"        # y coordinate for "Check" button in PI API Checker 1515, 1550, 1740 (0.98, 1, 1.12)
+PICHECK_V_X_COORD="890"       # x coordinate for "Show Verdict" button in PI API Checker  890, 890, 1015 (1, 1, 1.14)
+PICHECK_V_Y_COORD="180"       # y coordinate for "Show Verdict" button in PI API Checker 170, 180, 215 (0.94, 1, 1.21)
 
-AIC_Y_COORD="1410"            # y coordinate for "Check Device Integrity" button in Android Integrity Checker
-IAPI_Y_COORD="340"            # y coordinate for "Request A Universal Integrity Token With The Cloud Project" button in Integrity API
+MACHECK_Y_COORD="680"            # y coordinate for "Verify" button in Ma Integrity Check 670, 680, 760 (0.98, 1, 1.12)
+MACHECK_V_X_COORD="915"       # x coordinate for "Show Verdict" button in Ma Integrity Check 915, 915, 1040 (1, 1, 1.14)
+MACHECK_V_Y_COORD="185"       # y coordinate for "Show Verdict" button in Ma Integrity Check 180, 185, 220 (0.97, 1, 1.19)
 
 
 module_path="/data/adb/modules/playintegrityfix"
@@ -58,6 +56,7 @@ migrate_script="$script_path/migrate.sh"
 
 test_path="$1"; [ -z "$test_path" ] && test_path="$script_path"
 custom_bak="$test_path/custom.pif.bak"
+resolution=$(wm size)
 
 results="$test_path/pif-test-results.txt"; rm -f "$results"
 echo "results=$results"
@@ -66,6 +65,7 @@ echo "script_path=$script_path, test_path=$test_path" | tee -a "$results"
 echo "custom_json=$custom_json" | tee -a "$results"
 echo "MIGRATE_JSON=$MIGRATE_JSON, migrate_script=$migrate_script, DELETE_FAILED=$DELETE_FAILED" | tee -a "$results"
 echo "TEST_PI=$TEST_PI, PIF_JSON=$PIF_JSON" | tee -a "$results"
+echo "resolution=$resolution" | tee -a "$results"
 echo "" | tee -a "$results"
 
 log="$test_path/pif.log"
@@ -73,9 +73,9 @@ pi_xml="$test_path/pi.xml"
 
 pic="com.kblack.demo_play_integrity_api"
 spic="com.henrikherzig.playintegritychecker"
-picheck=gr.nikolasspyr.integritycheck
-aic="com.thend.integritychecker"
-iapi="com.test.integrity.api.qa.googlemanaged"
+picheck="gr.nikolasspyr.integritycheck"
+macheck="mtaate.checkintegrityma"
+
 
 integrities="NO_INTEGRITY MEETS_VIRTUAL_INTEGRITY MEETS_BASIC_INTEGRITY MEETS_DEVICE_INTEGRITY MEETS_STRONG_INTEGRITY"
 
@@ -249,52 +249,26 @@ function f_test_picheck()
   (( val = $? )); return $val
 }
 
-function f_test_aic()
+function f_test_macheck()
 {
   local json="$1"
   cp "$json" $custom_json
   f_remove_file "$log"; f_remove_file "$pi_xml"
 
-  f_start_activity $aic/$aic.MainActivity
+  f_start_activity $macheck/com.integrity.ui.MainActivity
   sleep 2
 
   f_kill_gms
-  f_tap $X_COORD $AIC_Y_COORD
+  f_tap $X_COORD $MACHECK_Y_COORD
 
   sleep $PI_WAIT_VERDICT
-  f_swipe $SWIPE_Y_COORD
+  f_tap $MACHECK_V_X_COORD $MACHECK_V_Y_COORD
   f_swipe $SWIPE_Y_COORD
   f_swipe $SWIPE_Y_COORD
   
   sleep 2
   f_dump_xml "$pi_xml"
-  f_kill_app $aic
-  logcat -d | grep -e PIF -e TrickyStore -e TEESimulator >> "$log"
-  
-  f_read_integrities "$pi_xml" "yes"
-  (( val = $? )); return $val
-}
-
-function f_test_iapi()
-{
-  local json="$1"
-  cp "$json" $custom_json
-  f_remove_file "$log"; f_remove_file "$pi_xml"
-
-  f_start_activity $iapi/com.dynamic.integrity.MainActivity
-  sleep 2
-
-  f_kill_gms
-  f_tap $X_COORD $IAPI_Y_COORD
-
-  sleep $PI_WAIT_VERDICT
-  f_swipe $SWIPE_Y_COORD
-  f_swipe $SWIPE_Y_COORD
-  f_swipe $SWIPE_Y_COORD
-  
-  sleep 2
-  f_dump_xml "$pi_xml"
-  f_kill_app $iapi
+  f_kill_app $macheck
   logcat -d | grep -e PIF -e TrickyStore -e TEESimulator >> "$log"
   
   f_read_integrities "$pi_xml" "yes"
@@ -324,15 +298,13 @@ function f_test_pi()
   [ -n "$prop" ] && echo "$prop" | tee -a "$results"
 
   case "$TEST_PI" in
-    TEST_AIC) f_test_aic "$json"
+    TEST_MACHECK) f_test_macheck "$json"
     ;;
-    TEST_IAPI) f_test_iapi "$json"
+   TEST_PICHECK) f_test_picheck "$json"
     ;;
     TEST_PIC) f_test_pic "$json"
     ;;
-    TEST_SPIC) f_test_spic "$json"
-    ;;
-    TEST_PICHECK|*) f_test_picheck "$json"
+    TEST_SPIC|*) f_test_spic "$json"
     ;;
   esac
   (( val = $? )); return $val
@@ -409,7 +381,7 @@ echo "" | tee -a "$results"
 f_remove_file "$custom_json"; cp "$custom_bak" "$custom_json" >/dev/null 2>&1
 f_kill_gms
 
-f_kill_app $pic; f_kill_app $spic; f_kill_app $picheck; f_kill_app $iapi; f_kill_app $aic
+f_kill_app $pic; f_kill_app $spic; f_kill_app $picheck; f_kill_app $macheck
 f_remove_file "$pi_xml"
 
 settings put system user_rotation "$orient"
